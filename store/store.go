b2566400a1d398c04ac5c758f941081362fc0af6
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-08-16 19:47:06 +0300

    Store init

---
+++ store/store.go
@@ -0,0 +1,32 @@
+package store
+
+import (
+	"context"
+	"github.com/slavik22/blogRestApi/model"
+	"gorm.io/gorm"
+	"log"
+)
+
+// Store contains all repositories
+type Store struct {
+	Db *gorm.DB
+
+	User UserRepo
+}
+
+// New creates new store
+func New(ctx context.Context, db *gorm.DB) (*Store, error) {
+	if err := db.AutoMigrate(&model.User{}); err != nil {
+		log.Println(err)
+	}
+
+	var store Store
+
+	// Init MySQL repositories
+	if db != nil {
+		store.Db = db
+		store.User = NewUserMysqlRepo(db)
+	}
+
+	return &store, nil
+}
